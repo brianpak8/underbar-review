2bbd1be5a4b72447095c283706f7b5948ac3cfc6
Hack Reactor Student <student@hackreactor.com>
2017-10-09 11:55:55 -0700

    filter implemented

--- src/underbar.js
+++ src/underbar.js
@@ -83,6 +83,14 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var pass = [];
+    _.each(collection, function(val) {
+      if ( test( val ) ) {
+        pass.push( val );
+      }
+    });
+
+    return pass;
   };
 
   // Return all elements of an array that don't pass a truth test.
